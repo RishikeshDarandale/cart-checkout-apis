09eab5d3a1f3ee6b3f49b12f7113c45dd0334257
Rishikesh Darandale <Rishikesh.Darandale@gmail.com>
2015-12-04 11:12:54 +0530

    [CART APIs] - Few more code to cart service

--- src/main/java/in/rishikeshdarandale/rest/resources/cart/CartResource.java
+++ src/main/java/in/rishikeshdarandale/rest/resources/cart/CartResource.java
@@ -17,14 +17,19 @@ package in.rishikeshdarandale.rest.resources.cart;
 
 import java.net.URI;
 
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import com.sun.media.jfxmedia.Media;
 
 import in.rishikeshdarandale.rest.model.cart.Cart;
 import in.rishikeshdarandale.rest.resources.AbstractResource;
@@ -45,5 +50,27 @@ public class CartResource extends AbstractResource {
     public Response create() {
         String id = cartService.create().getId();
         return Response.created(URI.create("http://localhost:8080/carts/" + id)).build();
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response get(String cartId) {
+        Cart cart = cartService.get(cartId);
+        if (cart != null) {
+            return created(cart.getId(), cart);
+        }else {
+            return foundNone();
+        }
+    }
+
+    @DELETE
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response delete(String cartId) {
+        Cart cart = cartService.get(cartId);
+        if (cart != null) {
+            return foundOne(cart);
+        }else {
+            return foundNone();
+        }
     }
 }
